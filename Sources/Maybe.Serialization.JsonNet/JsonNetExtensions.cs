ffcec2f73d6a401f9f2acdf13eabb033293080e2
Dani Michel <dani.michel@smartprimes.ch>
2014-06-24 14:55:24 +0200

    Non-maybe classes removed, general renaming from Belt to Maybe.

---
+++ Sources/Maybe.Serialization.JsonNet/JsonNetExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Maybe.Serialization.JsonNet
+{
+    using Newtonsoft.Json;
+
+    public static class JsonNetExtensions
+    {
+        public static JsonSerializerSettings ConfigureMaybe(this JsonSerializerSettings settings)
+        {
+            settings.Converters.Add(new MaybeConverter());
+
+            return settings;
+        }
+
+        public static JsonSerializer ConfigureMaybe(this JsonSerializer serializer)
+        {
+            serializer.Converters.Add(new MaybeConverter());
+
+            return serializer;
+        }
+    }
+}
