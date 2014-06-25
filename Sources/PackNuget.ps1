7ff6163e0b2354a2c95406f1f070508005475810
Dani Michel <dani.michel@smartprimes.ch>
2014-06-25 11:33:44 +0200

    Local feed directory set as output directory for nuget pack.

--- Sources/PackNuget.ps1
+++ Sources/PackNuget.ps1
@@ -1,12 +1,12 @@
 
 Push-Location Maybe
 
-nuget pack -Build -Prop Configuration=Release 
+nuget pack -Build -Prop Configuration=Release -Output ..\..\..\LocalNugetFeed
 
 Pop-Location
 
 Push-Location Maybe.Serialization.JsonNet
 
-nuget pack -Build -Prop Configuration=Release 
+nuget pack -Build -Prop Configuration=Release -Output ..\..\..\LocalNugetFeed
 
 Pop-Location
