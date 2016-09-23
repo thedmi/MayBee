58e0bcdb13077bf6d61beefc9ce0ad6cbb060b17
Dani Michel <dani.michel@softfactors.com>
2016-09-23 16:48:30 +0200

    Build script updated to work with nuget.exe v3.

--- Sources/PackNuget.ps1
+++ Sources/PackNuget.ps1
@@ -1,12 +1,12 @@
 
 Push-Location MayBee
 
-nuget pack -Build -Prop Configuration=Release -Output ..\..\..\LocalNugetFeed
+nuget pack MayBee.csproj -Build -Prop Configuration=Release -Output ..\..\..\LocalNugetFeed
 
 Pop-Location
 
 Push-Location MayBee.Serialization.JsonNet
 
-nuget pack -Build -Prop Configuration=Release -Output ..\..\..\LocalNugetFeed
+nuget pack MayBee.Serialization.JsonNet.csproj -Build -Prop Configuration=Release -Output ..\..\..\LocalNugetFeed
 
 Pop-Location
