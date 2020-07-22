f81120533796296ceb4930a3f2e581359750237e
Dani Michel <daniel.michel@geopraevent.ch>
2020-07-22 16:41:55 +0200

    Binary backwards-compatibility with 2.x restored

--- Sources/MayBee.Serialization.JsonNet/JsonNetExtensions.cs
+++ Sources/MayBee.Serialization.JsonNet/JsonNetExtensions.cs
@@ -4,13 +4,23 @@
 
     public static class JsonNetExtensions
     {
-        public static JsonSerializerSettings ConfigureMaybe(this JsonSerializerSettings settings, SerializationFormat format = SerializationFormat.Nullable)
+        public static JsonSerializerSettings ConfigureMaybe(this JsonSerializerSettings settings)
+        {
+            return ConfigureMaybe(settings, SerializationFormat.Nullable);
+        }
+        
+        public static JsonSerializerSettings ConfigureMaybe(this JsonSerializerSettings settings, SerializationFormat format)
         {
             settings.Converters.Add(GetConverter(format));
             return settings;
         }
+        
+        public static JsonSerializer ConfigureMaybe(this JsonSerializer settings)
+        {
+            return ConfigureMaybe(settings, SerializationFormat.Nullable);
+        }
 
-        public static JsonSerializer ConfigureMaybe(this JsonSerializer serializer, SerializationFormat format = SerializationFormat.Nullable)
+        public static JsonSerializer ConfigureMaybe(this JsonSerializer serializer, SerializationFormat format)
         {
             serializer.Converters.Add(GetConverter(format));
             return serializer;
---
+++ Sources/MayBee.Serialization.JsonNet/MaybeConverter.cs
@@ -0,0 +1,31 @@
+﻿namespace MayBee.Serialization.JsonNet
+{
+    using System;
+
+    using Newtonsoft.Json;
+
+    [Obsolete("Use MaybeAsArrayConverter instead for backwards compatibility, or switch to MaybeAsNullableConverter if desired (not compatible)")]
+    public class MaybeConverter : JsonConverter
+    {
+        private readonly MaybeAsArrayConverter _arrayConverter = new MaybeAsArrayConverter();
+
+        public override bool CanRead => _arrayConverter.CanRead;
+
+        public override bool CanWrite => _arrayConverter.CanWrite;
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            _arrayConverter.WriteJson(writer, value, serializer);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return _arrayConverter.ReadJson(reader, objectType, existingValue, serializer);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return _arrayConverter.CanConvert(objectType);
+        }
+    }
+}
